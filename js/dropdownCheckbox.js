0ca3c9fb6b3df4943736e8327934004783b587e9
SandroOrsiGit <118964227+SandroOrsiGit@users.noreply.github.com>
2022-12-08 14:59:28 +0100

    werk aan verhuur

---
+++ js/dropdownCheckbox.js
@@ -0,0 +1,7 @@
+let checkList = document.getElementById('checklist');
+document.getElementById('anchor').onclick = function() {
+  if (checkList.classList.contains('visible'))
+    checkList.classList.remove('visible');
+  else
+    checkList.classList.add('visible');
+}
