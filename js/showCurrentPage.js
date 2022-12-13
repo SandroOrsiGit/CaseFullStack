1d3222ebefb88d772d7c4891ca45cd5fd976ba26
Sandro Orsi <118964227+SandroOrsiGit@users.noreply.github.com>
2022-12-13 13:10:57 +0100

    image slider gecentreerd

--- js/showCurrentPage.js
+++ js/showCurrentPage.js
@@ -1,21 +1,24 @@
 "use strict";
-let currentUrl = window.location.href
+let currentUrl = window.location.href;
 
-if(currentUrl.indexOf('index.html')!=-1){
-    document.getElementById('index').style.transform = 'scale(1.5)'
-    document.getElementById('index').style.textDecoration = 'underline'
-    document.getElementById('index').style.color = '#f27289'
-}else if(currentUrl.indexOf('flavours.html')!=-1){
-    document.getElementById('flavours').style.transform = 'scale(1.5)'
-    document.getElementById('flavours').style.textDecoration = 'underline'
-    document.getElementById('flavours').style.color = '#f27289'
-}else if(currentUrl.indexOf('verhuur.html')!=-1){
-    document.getElementById('verhuur').style.transform = 'scale(1.5)'
-    document.getElementById('verhuur').style.textDecoration = 'underline'
-    document.getElementById('verhuur').style.color = '#f27289'
-}else if(currentUrl.indexOf('contact.html')!=-1){
-    document.getElementById('contact').style.transform = 'scale(1.5)'
-    document.getElementById('contact').style.textDecoration = 'underline'
-    document.getElementById('contact').style.color = '#f27289'
-
+if (currentUrl.indexOf("index.html") != -1) {
+	document.getElementById("index").style.transform = "scale(1.5)";
+	document.getElementById("index").style.textDecoration = "underline";
+	document.getElementById("index").style.color = "#f27289";
+} else if (currentUrl.indexOf("flavours.html") != -1) {
+	document.getElementById("flavours").style.transform = "scale(1.5)";
+	document.getElementById("flavours").style.textDecoration = "underline";
+	document.getElementById("flavours").style.color = "#f27289";
+} else if (currentUrl.indexOf("verhuur.html") != -1) {
+	document.getElementById("verhuur").style.transform = "scale(1.5)";
+	document.getElementById("verhuur").style.textDecoration = "underline";
+	document.getElementById("verhuur").style.color = "#f27289";
+} else if (currentUrl.indexOf("contact.html") != -1) {
+	document.getElementById("contact").style.transform = "scale(1.5)";
+	document.getElementById("contact").style.textDecoration = "underline";
+	document.getElementById("contact").style.color = "#f27289";
+} else {
+	document.getElementById("index").style.transform = "scale(1.5)";
+	document.getElementById("index").style.textDecoration = "underline";
+	document.getElementById("index").style.color = "#f27289";
 }
