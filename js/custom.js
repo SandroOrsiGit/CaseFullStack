b4c4346b4f87ee79db76c1befcefcb7081da04a2
Sandro Orsi <118964227+SandroOrsiGit@users.noreply.github.com>
2022-12-14 14:36:20 +0100

    finalized

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,15 @@
-$(function() {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrow:true,
-        autoplay:false,
-        autoplaySpeed:2000,
-        dots: false,
-        centerMode: true,
-        centerPadding: '0',
-          });
-      });
+"use strict";
+
+$(function () {
+	$(".multiple-items").slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrow: true,
+		autoplay: false,
+		autoplaySpeed: 2000,
+		dots: false,
+		centerMode: true,
+		centerPadding: "0",
+	});
+});
